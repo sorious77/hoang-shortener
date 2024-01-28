ad7c5c01f8253e27e6352f5fabb54faacb26c389
sorious77 <sorious77@naver.com>
2024-01-28 17:21:55 +0900

    빌드 오류 해결

--- src/components/ui/layout.tsx
+++ src/components/ui/layout.tsx
@@ -1,9 +1,11 @@
 import { ReactNode } from "react";
 
-export default (props: { children: ReactNode }) => {
+const Layout = (props: { children: ReactNode }) => {
   return (
     <main className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white">
       {props.children}
     </main>
   );
 };
+
+export default Layout;
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,7 +9,7 @@ const Error = () => {
       <section className="max-w-2xl mx-auto mt-10 px-4 py-6 text-center">
         <h1 className="font-semibold text-6xl">404</h1>
         <p className="mt-4 text-xl">
-          Sorry, the page you're looking for cannot be found.
+          Sorry, the page you&apos;re looking for cannot be found.
         </p>
         <Button
           className="mt-6 bg-transparent border-2 border-white text-white hover:bg-white hover:text-blue-500 transition-colors duration-200 py-2 px-6 rounded-full"
